4b02d32ac2228b7824282d5ca74e0d1836eb95ab
aaronviets <adviets@uwm.edu>
2016-05-04 09:51:33 -0500

    Update unary_log10.c

--- gst/unary/unary_log10.c
+++ gst/unary/unary_log10.c
@@ -9,7 +9,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
@@ -20,79 +20,138 @@
 
 #include <unary_base.h>
 
-#include <math.h>
+#define TYPE_UNARY_LOG10 \
+	(unary_log10_get_type())
+#define UNARY_LOG10(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST((obj),TYPE_UNARY_LOG10,UnaryLog10))
+#define UNARY_LOG10_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_CAST((klass),TYPE_UNARY_LOG10,UnaryLog10Class))
+#define IS_PLUGIN_TEMPLATE(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE((obj),TYPE_UNARY_LOG10))
+#define IS_PLUGIN_TEMPLATE_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_TYPE((klass),TYPE_UNARY_LOG10))
 
+typedef struct _UnaryLog10 UnaryLog10;
+typedef struct _UnaryLog10Class UnaryLog10Class;
 
-static GstFlowReturn
-transform_ip (GstBaseTransform * trans, GstBuffer * buf)
+GType
+unary_log10_get_type(void);
+
+struct _UnaryLog10
 {
-  GstAudioFilter *audiofilter = GST_AUDIO_FILTER (trans);
-  GstAudioFormat format = audiofilter->info.finfo->format;
+	UnaryBase unary_base;
+};
 
-  GstMapInfo info;
-  gst_buffer_map (buf, &info, GST_MAP_READ);
-  gpointer data = info.data;
-  gpointer data_end = data + info.size;
-  gst_buffer_unmap (buf, &info);
+struct _UnaryLog10Class 
+{
+	UnaryBaseClass parent_class;
+};
 
-  if (format >= GST_AUDIO_FORMAT_F64) {
-    double *ptr, *end = data_end;
-    for (ptr = data; ptr < end; ptr++)
-      *ptr = log10 (*ptr);
-  } else if (format >= GST_AUDIO_FORMAT_F32) {
-    float *ptr, *end = data_end;
-    for (ptr = data; ptr < end; ptr++)
-      *ptr = log10f (*ptr);
-  } else {
-    g_assert_not_reached ();
-  }
+static GstFlowReturn transform_ip(GstBaseTransform *trans, GstBuffer *buf);
 
-  return GST_FLOW_OK;
+/* Initialize the plugin's class */
+static void
+unary_log10_class_init(gpointer klass, gpointer klass_data)
+{
+	GstBaseTransformClass *basetransform_class = GST_BASE_TRANSFORM_CLASS(klass);
+
+	gst_element_class_set_details_simple(GST_ELEMENT_CLASS(klass),
+		"Logarithm base 10",
+		"Filter/Audio",
+		"Calculate logarithm base 10, y = log_10 x",
+		"Leo Singer <leo.singer@ligo.org>, Aaron Viets <aaron.viets@ligo.org>");
+
+	basetransform_class -> transform_ip = GST_DEBUG_FUNCPTR(transform_ip);
+	basetransform_class -> set_caps = GST_DEBUG_FUNCPTR(set_caps);
 }
-
 
 /*
- * ============================================================================
+ * GstBaseTransform vmethod implementations
  *
- *                                Type Support
- *
- * ============================================================================
+ * An in-place transform
+ * really does the same thing as the chain function
  */
 
-
-static void
-base_init (gpointer class)
+static GstFlowReturn
+transform_ip(GstBaseTransform * trans, GstBuffer * buf)
 {
-  gst_element_class_set_details_simple (GST_ELEMENT_CLASS (class),
-      "Logarithm base 10",
-      "Filter/Audio",
-      "Calculate logarithm base 10, y = log_10 x",
-      "Aaron Viets <aaron.viets@ligo.org>, Leo Singer <leo.singer@ligo.org>");
-}
+	UnaryLog10* element = UNARY_LOG10(trans);
+	int bits = element -> unary_base.bits;
+	int is_complex = element -> unary_base.is_complex;
 
+	/* Debugging
+	 *
+	 * GstObject* element_gstobj = GST_OBJECT(trans);
+	 * int channels = element -> unary_base.channels;
+	 * int rate = element -> unary_base.rate;
+	 * g_print("[%s]: passing GstBuffer: ", element_gstobj->name);
+	 * g_print("%d channels, ", channels);
+	 * g_print("%d bits, ", bits);
+	 * g_print("rate: %d, ", rate);
+	 */
 
-static void
-class_init (gpointer class, gpointer class_data)
-{
-  GstBaseTransformClass *basetransform_class = GST_BASE_TRANSFORM_CLASS (class);
-  basetransform_class->transform_ip = GST_DEBUG_FUNCPTR (transform_ip);
+	GstMapInfo info;
+	if(!gst_buffer_map(buf, &info, GST_MAP_READWRITE)) {
+		GST_ERROR_OBJECT(trans, "gst_buffer_map failed\n");
+	}
+	gpointer data = info.data;
+	gpointer data_end = data + info.size;
+
+	if(is_complex == 1) {
+
+		if(bits == 128) {
+			/* g_print("COMPLEX FLOAT128\n"); */
+			double complex *ptr, *end = data_end;
+			for(ptr = data; ptr < end; ptr++) {
+				*ptr = clog(*ptr) / log(10);
+			}
+		} else if(bits == 64) {
+			/* g_print("COMPLEX FLOAT64\n"); */
+			float complex *ptr, *end = data_end;
+			for(ptr = data; ptr < end; ptr++) {
+				*ptr = clogf(*ptr) / log(10);
+			}
+		} else {
+			g_assert_not_reached();
+		}
+	} else if(is_complex == 0) {
+
+		if(bits == 64) {
+			/* g_print("REAL FLOAT64\n"); */
+			double *ptr, *end = data_end;
+			for(ptr = data; ptr < end; ptr++) {
+				*ptr = log10(*ptr);
+			}
+		} else if(bits == 32) {
+			/* g_print("REAL FLOAT32\n"); */
+			float *ptr, *end = data_end;
+			for(ptr = data; ptr < end; ptr++) {
+				*ptr = log10f(*ptr);
+			}
+		} else {
+			g_assert_not_reached();
+		}
+	} else {
+		g_assert_not_reached();
+	}
+	gst_buffer_unmap(buf, &info);
+	return GST_FLOW_OK;
 }
 
 
 GType
-unary_log10_get_type (void)
+unary_log10_get_type(void)
 {
-  static GType type = 0;
+	static GType type = 0;
 
-  if (!type) {
-    static const GTypeInfo info = {
-      .class_size = sizeof (UnaryBaseClass),
-      .base_init = base_init,
-      .class_init = class_init,
-      .instance_size = sizeof (UnaryBase),
-    };
-    type = g_type_register_static (UNARY_BASE_TYPE, "UnaryLog10", &info, 0);
-  }
+	if(!type) {
+		static const GTypeInfo info = {
+			.class_size = sizeof(UnaryBaseClass),
+			.class_init = unary_log10_class_init,
+			.instance_size = sizeof(UnaryBase),
+		};
+		type = g_type_register_static(UNARY_BASE_TYPE, "UnaryLog10", &info, 0);
+	}
 
-  return type;
+	return type;
 }
