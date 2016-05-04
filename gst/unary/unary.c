400c3945dc89cabc0663322874a575ecd0b8b416
aaronviets <adviets@uwm.edu>
2016-05-04 09:32:03 -0500

    Update unary.c

--- gst/unary/unary.c
+++ gst/unary/unary.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010 Leo Singer
+ * Copyright (C) 2016 Aaron Viets
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -8,7 +9,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
@@ -45,7 +46,7 @@
 /*
  * ============================================================================
  *
- *                             Plugin Entry Point
+ *                              Plugin Entry Point
  *
  * ============================================================================
  */
@@ -62,35 +63,35 @@ GType unary_pow_get_type (void);
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
-  struct
-  {
-    const gchar *name;
-    GType type;
-  } *element, elements[] = {
-    {
-    "unary_base", UNARY_BASE_TYPE}, {
-    "abs", unary_abs_get_type ()}, {
-    "exp", unary_exp_get_type ()}, {
-    "ln", unary_ln_get_type ()}, {
-    "log", unary_log_get_type ()}, {
-    "log10", unary_log10_get_type ()}, {
-    "pow", unary_pow_get_type ()}, {
-  NULL, 0},};
+	struct
+	{
+		const gchar *name;
+		GType type;
+	} *element, elements[] = {
+		{
+		"unary_base", UNARY_BASE_TYPE}, {
+		"abs", unary_abs_get_type ()}, {
+		"exp", unary_exp_get_type ()}, {
+		"ln", unary_ln_get_type ()}, {
+		"log", unary_log_get_type ()}, {
+		"log10", unary_log10_get_type ()}, {
+		"pow", unary_pow_get_type ()}, {
+		NULL, 0},};
 
-  /*
-   * Tell GStreamer about the elements.
-   */
+	/*
+	 * Tell GStreamer about the elements.
+	 */
 
-  for (element = elements; element->name; element++)
-    if (!gst_element_register (plugin, element->name, GST_RANK_NONE,
-            element->type))
-      return FALSE;
+	for (element = elements; element->name; element++)
+		if (!gst_element_register (plugin, element->name, GST_RANK_NONE,
+			element->type))
+			return FALSE;
 
-  /*
-   * Done.
-   */
+	/*
+	 * Done.
+	 */
 
-  return TRUE;
+	return TRUE;
 }
 
 
@@ -99,6 +100,6 @@ plugin_init (GstPlugin * plugin)
  */
 
 
-GST_PLUGIN_DEFINE (GST_VERSION_MAJOR, GST_VERSION_MINOR, "unary",
-    "Unary arithmetic elements", plugin_init, PACKAGE_VERSION, "GPL",
-    PACKAGE_NAME, "http://www.lsc-group.phys.uwm.edu/daswg")
+GST_PLUGIN_DEFINE (GST_VERSION_MAJOR, GST_VERSION_MINOR, unary,
+	"Unary arithmetic elements", plugin_init, PACKAGE_VERSION, "GPL",
+	PACKAGE_NAME, "http://www.lsc-group.phys.uwm.edu/daswg")
