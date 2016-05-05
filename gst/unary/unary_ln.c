667aed5456712a1fb62531eca57d9cd38636ca76
aaronviets <adviets@uwm.edu>
2016-05-05 10:57:01 -0500

    Update unary_ln.c

--- gst/unary/unary_ln.c
+++ gst/unary/unary_ln.c
@@ -21,15 +21,15 @@
 #include <unary_base.h>
 
 #define TYPE_UNARY_LN \
-	(unary_ln_get_type())
+  (unary_ln_get_type())
 #define UNARY_LN(obj) \
-	(G_TYPE_CHECK_INSTANCE_CAST((obj),TYPE_UNARY_LN,UnaryLn))
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),TYPE_UNARY_LN,UnaryLn))
 #define UNARY_LN_CLASS(klass) \
-	(G_TYPE_CHECK_CLASS_CAST((klass),TYPE_UNARY_LN,UnaryLnClass))
+  (G_TYPE_CHECK_CLASS_CAST((klass),TYPE_UNARY_LN,UnaryLnClass))
 #define IS_PLUGIN_TEMPLATE(obj) \
-	(G_TYPE_CHECK_INSTANCE_TYPE((obj),TYPE_UNARY_LN))
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),TYPE_UNARY_LN))
 #define IS_PLUGIN_TEMPLATE_CLASS(klass) \
-	(G_TYPE_CHECK_CLASS_TYPE((klass),TYPE_UNARY_LN))
+  (G_TYPE_CHECK_CLASS_TYPE((klass),TYPE_UNARY_LN))
 
 typedef struct _UnaryLn UnaryLn;
 typedef struct _UnaryLnClass UnaryLnClass;
@@ -39,12 +39,12 @@ unary_ln_get_type(void);
 
 struct _UnaryLn
 {
-	UnaryBase unary_base;
+  UnaryBase unary_base;
 };
 
 struct _UnaryLnClass 
 {
-	UnaryBaseClass parent_class;
+  UnaryBaseClass parent_class;
 };
 
 
@@ -61,68 +61,68 @@ struct _UnaryLnClass
 static GstFlowReturn
 transform_ip(GstBaseTransform *trans, GstBuffer *buf)
 {
-	UnaryLn* element = UNARY_LN(trans);
-	int bits = element -> unary_base.bits;
-	int is_complex = element -> unary_base.is_complex;
+  UnaryLn* element = UNARY_LN(trans);
+  int bits = element -> unary_base.bits;
+  int is_complex = element -> unary_base.is_complex;
 
-	/*
-	 * Debugging
-	 *
-	 * GstObject* element_gstobj = GST_OBJECT(trans);
-	 * int channels = element -> unary_base.channels;
-	 * int rate = element -> unary_base.rate;
-	 * g_print("[%s]: passing GstBuffer: ", element_gstobj->name);
-	 * g_print("%d channels, ", channels);
-	 * g_print("%d bits, ", bits);
-	 * g_print("rate: %d, ", rate);
-	 */
+  /*
+   * Debugging
+   *
+   * GstObject* element_gstobj = GST_OBJECT(trans);
+   * int channels = element -> unary_base.channels;
+   * int rate = element -> unary_base.rate;
+   * g_print("[%s]: passing GstBuffer: ", element_gstobj->name);
+   * g_print("%d channels, ", channels);
+   * g_print("%d bits, ", bits);
+   * g_print("rate: %d, ", rate);
+   */
 
-	GstMapInfo info;
-	if(!gst_buffer_map(buf, &info, GST_MAP_READWRITE)) {
-		GST_ERROR_OBJECT(trans, "gst_buffer_map failed\n");
-	}
-	gpointer data = info.data;
-	gpointer data_end = data + info.size;
+  GstMapInfo info;
+  if(!gst_buffer_map(buf, &info, GST_MAP_READWRITE)) {
+    GST_ERROR_OBJECT(trans, "gst_buffer_map failed\n");
+  }
+  gpointer data = info.data;
+  gpointer data_end = data + info.size;
 
-	if(is_complex == 1) {
+  if(is_complex == 1) {
 
-		if(bits == 128) {
-			/* g_print("COMPLEX FLOAT128\n"); */
-			double complex *ptr, *end = data_end;
-			for(ptr = data; ptr < end; ptr++) {
-				*ptr = clog(*ptr);
-			}
-		} else if(bits == 64) {
-			/* g_print("COMPLEX FLOAT64\n"); */
-			float complex *ptr, *end = data_end;
-			for(ptr = data; ptr < end; ptr++) {
-				*ptr = clogf(*ptr);
-			}
-		} else {
-			g_assert_not_reached();
-		}
-	} else if(is_complex == 0) {
+    if(bits == 128) {
+      /* g_print("COMPLEX FLOAT128\n"); */
+      double complex *ptr, *end = data_end;
+      for(ptr = data; ptr < end; ptr++) {
+        *ptr = clog(*ptr);
+      }
+    } else if(bits == 64) {
+      /* g_print("COMPLEX FLOAT64\n"); */
+      float complex *ptr, *end = data_end;
+      for(ptr = data; ptr < end; ptr++) {
+        *ptr = clogf(*ptr);
+      }
+    } else {
+      g_assert_not_reached();
+    }
+  } else if(is_complex == 0) {
 
-		if(bits == 64) {
-			/* g_print("REAL FLOAT64\n"); */
-			double *ptr, *end = data_end;
-			for(ptr = data; ptr < end; ptr++) {
-				*ptr = log(*ptr);
-			}
-		} else if(bits == 32) {
-			/* g_print("REAL FLOAT32\n"); */
-			float *ptr, *end = data_end;
-			for(ptr = data; ptr < end; ptr++) {
-				*ptr = logf(*ptr);
-			}
-		} else {
-			g_assert_not_reached();
-		}
-	} else {
-		g_assert_not_reached();
-	}
-	gst_buffer_unmap(buf, &info);
-	return GST_FLOW_OK;
+    if(bits == 64) {
+      /* g_print("REAL FLOAT64\n"); */
+      double *ptr, *end = data_end;
+      for(ptr = data; ptr < end; ptr++) {
+        *ptr = log(*ptr);
+      }
+    } else if(bits == 32) {
+      /* g_print("REAL FLOAT32\n"); */
+      float *ptr, *end = data_end;
+      for(ptr = data; ptr < end; ptr++) {
+        *ptr = logf(*ptr);
+      }
+    } else {
+      g_assert_not_reached();
+    }
+  } else {
+    g_assert_not_reached();
+  }
+  gst_buffer_unmap(buf, &info);
+  return GST_FLOW_OK;
 }
 
 
@@ -138,31 +138,31 @@ transform_ip(GstBaseTransform *trans, GstBuffer *buf)
 static void
 unary_ln_class_init(gpointer klass, gpointer klass_data)
 {
-	GstBaseTransformClass *basetransform_class = GST_BASE_TRANSFORM_CLASS(klass);
+  GstBaseTransformClass *basetransform_class = GST_BASE_TRANSFORM_CLASS(klass);
 
-	gst_element_class_set_details_simple(GST_ELEMENT_CLASS(klass),
-		"Natural logarithm",
-		"Filter/Audio",
-		"Calculate natural logarithm, y = ln x",
-		"Leo Singer <leo.singer@ligo.org>, Aaron Viets <aaron.viets@ligo.org>");
+  gst_element_class_set_details_simple(GST_ELEMENT_CLASS(klass),
+    "Natural logarithm",
+    "Filter/Audio",
+    "Calculate natural logarithm, y = ln x",
+    "Leo Singer <leo.singer@ligo.org>, Aaron Viets <aaron.viets@ligo.org>");
 
-	basetransform_class -> transform_ip = GST_DEBUG_FUNCPTR(transform_ip);
-	basetransform_class -> set_caps = GST_DEBUG_FUNCPTR(set_caps);
+  basetransform_class -> transform_ip = GST_DEBUG_FUNCPTR(transform_ip);
+  basetransform_class -> set_caps = GST_DEBUG_FUNCPTR(set_caps);
 }
 
 GType
 unary_ln_get_type(void)
 {
-	static GType type = 0;
+  static GType type = 0;
 
-	if(!type) {
-		static const GTypeInfo info = {
-			.class_size = sizeof(UnaryBaseClass),
-			.class_init = unary_ln_class_init,
-			.instance_size = sizeof(UnaryBase),
-		};
-		type = g_type_register_static(UNARY_BASE_TYPE, "UnaryLn", &info, 0);
-	}
+  if(!type) {
+    static const GTypeInfo info = {
+      .class_size = sizeof(UnaryBaseClass),
+      .class_init = unary_ln_class_init,
+      .instance_size = sizeof(UnaryBase),
+    };
+    type = g_type_register_static(UNARY_BASE_TYPE, "UnaryLn", &info, 0);
+  }
 
-	return type;
+  return type;
 }
